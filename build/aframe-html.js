5a144fa86bf6f3030b61e0b232de1da3f3eb5539
Vincent Fretin <vincent.fretin@gmail.com>
2026-02-15 10:59:02 +0100

    Build dist

--- build/aframe-html.js
+++ build/aframe-html.js
@@ -80,16 +80,7 @@
 			this.dom = dom;
 
 			this.anisotropy = 16;
-			if ( THREE.REVISION === '147' ) { // Keep compatibility with aframe 1.4.2
-
-				this.encoding = three.sRGBEncoding;
-
-			} else {
-
-				this.colorSpace = three.SRGBColorSpace;
-
-			}
-
+			this.colorSpace = three.SRGBColorSpace;
 			this.minFilter = three.LinearFilter;
 			this.magFilter = three.LinearFilter;
 			this.generateMipmaps = false;
